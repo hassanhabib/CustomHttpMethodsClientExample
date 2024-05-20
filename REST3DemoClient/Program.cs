67cef4233bc78f81a46334e8e7139c4e4d5840db
Hassan Habib <hassanhabib@live.com>
2024-05-19 18:34:44 -0700

    Add project files.

---
+++ REST3DemoClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using RESTFulSense.Clients;
+
+namespace REST3DemoClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var restfulHttpClient = new RESTFulApiClient();
+            restfulHttpClient.BaseAddress = new Uri("https://localhost:7119/");
+
+            Student response =
+                await restfulHttpClient.SendHttpRequestAsync<Student>(
+                    method: "CUSTOM",
+                    relativeUrl: "api/home",
+                    cancellationToken: new CancellationToken());
+
+            Console.WriteLine(response.Id);
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+    }
+}
